6c573549ddb928348a14782f57435983582b5360
Devashish Sarmah <devashish.dev.sarmah@gmail.com>
2024-06-20 22:31:57 +0530

    fix: postmodule providers

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
-import { GeminiAiService } from "../../services/gemini-ai/gemini-ai.service";
+import { GeminiAiService } from '../../services/gemini-ai/gemini-ai.service';
+import { StableDiffusionService } from '../../services/stable-diffusion/stable-diffusion.service';
 
 @Module({
   controllers: [PostController],
-  providers: [PostService, GeminiAiService],
+  providers: [PostService, GeminiAiService, StableDiffusionService],
 })
 export class PostModule {}
