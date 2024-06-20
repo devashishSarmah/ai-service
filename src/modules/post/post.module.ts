2693f2e9c9bee62938b8c757d06a6c28e55ca4b3
Devashish Sarmah <devashish.dev.sarmah@gmail.com>
2024-06-20 22:39:05 +0530

    fix: postmodule providers

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -5,8 +5,10 @@ import { GeminiAiService } from '../../services/gemini-ai/gemini-ai.service';
 import { StableDiffusionService } from '../../services/stable-diffusion/stable-diffusion.service';
 import { AiUserService } from '../ai-user/ai-user.service';
 import { CloudinaryService } from '../../services/cloudinary/cloudinary.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [PostController],
   providers: [
     PostService,
