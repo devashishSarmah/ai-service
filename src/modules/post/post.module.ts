54f4ecfcb23e427a20d1c484d7209db47e91e6a8
Devashish Sarmah <devashish.dev.sarmah@gmail.com>
2024-06-20 22:35:27 +0530

    fix: postmodule providers

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -3,9 +3,17 @@ import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { GeminiAiService } from '../../services/gemini-ai/gemini-ai.service';
 import { StableDiffusionService } from '../../services/stable-diffusion/stable-diffusion.service';
+import { AiUserService } from '../ai-user/ai-user.service';
+import { CloudinaryService } from '../../services/cloudinary/cloudinary.service';
 
 @Module({
   controllers: [PostController],
-  providers: [PostService, GeminiAiService, StableDiffusionService],
+  providers: [
+    PostService,
+    GeminiAiService,
+    StableDiffusionService,
+    AiUserService,
+    CloudinaryService,
+  ],
 })
 export class PostModule {}
